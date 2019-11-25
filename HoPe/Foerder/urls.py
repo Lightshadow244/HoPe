835607db5cf65b907779bf43cc49db620517e01c
Lightshadow244 <richi.lightshadow@gmail.com>
2019-11-25 18:42:16 +0100

    supportus impressum

--- HoPe/Foerder/urls.py
+++ HoPe/Foerder/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.redirect, name='redirect'),
+    path('home', views.home, name='home'),
+    path('support_us', views.support_us, name='support_us'),
+    path('impressum', views.impressum, name='impressum'),
 ]
--- HoPe/Foerder/views.py
+++ HoPe/Foerder/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse
 from django.utils.translation import gettext as _
 
 # Create your views here.
-def index(request):
+def home(request):
     #output = _("Welcome to my site.")
     #return HttpResponse(output)
-    print(request.build_absolute_uri)
+    #print(request.build_absolute_uri)
     return render(request, 'Foerder/index.html')
+
+def support_us(request):
+    #output = _("Welcome to my site.")
+    #return HttpResponse(output)
+    #print(request.build_absolute_uri)
+    return render(request, 'Foerder/support_us.html')
+
+def impressum(request):
+    #output = _("Welcome to my site.")
+    #return HttpResponse(output)
+    #print(request.build_absolute_uri)
+    return render(request, 'Foerder/impressum.html')
+
+def redirect(request):
+    response = HttpResponse(status=302)
+    response['Location'] = 'home'
+    return response
