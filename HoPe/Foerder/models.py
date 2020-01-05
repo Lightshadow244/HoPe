a8833a90a509f92a1bf6a5aa821f3131ab453ab8
Lightshadow244 <richi.lightshadow@gmail.com>
2020-01-05 11:10:53 +0000

    Database

--- HoPe/Foerder/models.py
+++ HoPe/Foerder/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
+import os
 # Create your models here.
+class Event(models.Model):
+	title_german = models.CharField(max_length=200)
+	title_english = models.CharField(max_length=200)
+	text_german = models.TextField()
+	text_english = models.TextField()
+	pub_date = models.DateTimeField('date published')
+
+class Image(models.Model):
+	image = models.FileField(upload_to="static_prod/EventImages/", default="")
+	main = models.BooleanField(default=False)
+	whichEvent = models.ForeignKey(Event, on_delete=models.CASCADE, default=1)
