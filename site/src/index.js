b350d7dee5f2c0aad2bf748ec98e8be5949c47d1
Lightshadow244 <richi.lightshadow@gmail.com>
2023-07-04 17:53:00 +0200

    Create index.js

---
+++ site/src/index.js
@@ -0,0 +1,13 @@
+class Site extends React.Component {
+  render() {
+     return (
+      <div className="site">
+        Dies ist ein Test!
+      </div>
+    )
+}
+
+ReactDOM.render(
+  <Site />,
+  document.getElementById('root')
+);
